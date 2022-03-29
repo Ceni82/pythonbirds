bb62c193878c888829e67500c15508f2987afd10
Mateus Ceni <mateus.ceni@gmail.com>
2022-03-29 20:46:30 -0300

    criados atributo de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,20 @@
 class Pessoa:
-    def __init__(self, nome = None, idade=39):
+    def __init__(self, *filhos, nome = None, idade=39):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
+
     def cumprimentar(self):
-        return f'Ola {id(self)}'
+        return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa("Ceni")
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Mateus'
-    print(p.nome)
-    print(p.idade)
+    mateus = Pessoa(nome='Mateus')
+    ceni = Pessoa(mateus, nome='Ceni')
+    print(Pessoa.cumprimentar(mateus))
+    print(id(mateus))
+    print(mateus.cumprimentar())
+    print(mateus.nome)
+    print(mateus.idade)
+    for filho in mateus.filhos:
+        print(filho.nome)
+    print(mateus.filhos)
