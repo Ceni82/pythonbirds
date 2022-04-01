cf832deefeaa39bc15e8971e96c2e11ae24be8e2
Mateus Ceni <mateus.ceni@gmail.com>
2022-03-31 21:12:30 -0300

    criados atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *filhos, nome = None, idade=39):
         self.idade = idade
         self.nome = nome
@@ -6,6 +8,19 @@ class Pessoa:
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
+
+    # @staticmathod
+    # def metodo_estatico():
+    #     return 42
+    #
+    # @classmathod
+    # def nome_e_atributos_de_classe(cls):
+    #     return f'{cls} - olhos {cls.olhos}'
+
+
+
+
+
 
 if __name__ == '__main__':
     mateus = Pessoa(nome='Mateus')
@@ -19,5 +34,14 @@ if __name__ == '__main__':
         print(filho.nome)
     mateus.sobrenome = 'Thor'
     del mateus.filhos
+    mateus.olhos = 1
+    del mateus.olhos
     print(mateus.__dict__)
     print(ceni.__dict__)
+    Pessoa.olhos= 3
+    print(Pessoa.olhos)
+    print(mateus.olhos)
+    print(ceni.olhos)
+    print(id(Pessoa.olhos), id(mateus.olhos), id(ceni.olhos))
+    # print(Pessoa.metodo_estatico(), mateus.metodo_estatico))
+    # print(Pessoa.nome_e_atributos_de_classe(), mateus.nome_e_atributos_de_classe())
